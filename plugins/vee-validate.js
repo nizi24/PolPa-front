3e856de636934ee42b515b9fd583821173c7953c
nizi <failaforcontact@gmail.com>
2020-07-01 10:33:38 +0900

    add castom_validate

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import { ValidationProvider, ValidationObserver, extend, localize } from 'vee-validate'
 import * as originalRules from 'vee-validate/dist/rules'
 import ja from 'vee-validate/dist/locale/ja.json'
+import * as CustomRules from '~/utils/validation-custom-rules.js'
 
 let rule
 for (rule in originalRules) {
   extend(rule, {
     ...originalRules[rule] // eslint-disable-line
   })
+}
+
+for (const rule in CustomRules) {
+  extend(rule, CustomRules[rule])
 }
 
 localize('ja', ja)
