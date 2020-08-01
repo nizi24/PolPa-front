a43e3f9d7ee10b3a29ce0f07179d1d721377c332
nizi <failaforcontact@gmail.com>
2020-08-01 23:12:51 +0900

    利用規約&プラポリ&フッターを追加

---
+++ pages/contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="8">
+      <v-card id="about-card">
+        <v-card-title>
+          <span id="about-card-title">お問い合わせ</span>
+        </v-card-title>
+        <v-card-text>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+#about-card {
+  padding: 20px;
+}
+
+#about-card-title {
+  font-size: 1.4em;
+}
+</style>
